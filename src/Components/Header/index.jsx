879eb5ee923ff3bc443dfae1c2ba633d2ad861d3
Juan Camilo Clement Santacruz <juancamiloclement@gmail.com>
2023-07-14 13:56:13 -0500

    agrega despliegue barra de busqueda y responsive 500px

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -1,22 +1,33 @@
 import './Header.scss';
+import { useState } from 'react';
 import {AiOutlineSearch} from 'react-icons/ai';
 import {FiBell,FiSun} from 'react-icons/fi';
 import {RxHamburgerMenu} from 'react-icons/rx';
 
 const Header = () => {
 
+  const [showSearchBar,setShowSearchBar] = useState(false);
+
+  const handleDisplaySearch = () => setShowSearchBar(!showSearchBar);
+
   return (
     <>
         <header>
             <img src="src/assets/logo-white.png" alt="Nuron logo" />
             <section className='buttonsContainer'>
-                <div className='iconContainer'><AiOutlineSearch/></div>
+                <div className='iconContainer' onClick={handleDisplaySearch}><AiOutlineSearch/></div>
+
                 <div><button>Wallet connect</button></div>
                 <div className='iconContainer'><FiBell/></div>
                 <div className='iconContainer'><RxHamburgerMenu/></div>
                 <div className='iconContainer'><FiSun/></div>
             </section>
         </header>
+        {showSearchBar && 
+        <div className='searchBarContainer'>
+          <input type="text" placeholder='Search...' className='searchInput'/>
+          <button className='searchButton'><AiOutlineSearch/></button>
+        </div>}
     </>
   )
 }
