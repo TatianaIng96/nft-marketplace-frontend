cb04298ef65d1f9115174930cc207cdf83f8e768
Juan Camilo Clement Santacruz <juancamiloclement@gmail.com>
2023-07-15 15:03:34 -0500

    completa responsive design

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -4,6 +4,7 @@ import {AiOutlineSearch} from 'react-icons/ai';
 import {FiBell, FiSun} from 'react-icons/fi';
 import {RxHamburgerMenu} from 'react-icons/rx';
 import {HiOutlineX} from 'react-icons/hi';
+import {BiChevronDown} from 'react-icons/bi';
 
 const Header = () => {
 
@@ -20,16 +21,36 @@ const Header = () => {
   return (
     <>
       {showHeaderOverMenu && 
+      <div>
       <header>
         <img src="src/assets/logo-white.png" alt="Nuron logo" />
+        <span className='menuInHeader home'>Home <BiChevronDown/></span>
+        <span className='menuInHeader'>About</span>
+        <span className='menuInHeader'>Explore <BiChevronDown/></span>
+        <span className='menuInHeader'>Pages <BiChevronDown/></span>
+        <span className='menuInHeader'>Blog <BiChevronDown/></span>
+        <span className='menuInHeader'>Contact</span>
         <section className='buttonsContainer'>
-            <div className='iconContainer' onClick={handleDisplaySearch}><AiOutlineSearch/></div>
+            <div className='iconContainer' id='searchShower'onClick={handleDisplaySearch}><AiOutlineSearch/></div>
+            <div className='searchInHeaderContainer'>
+              <input type="text" placeholder='Search...' className='searchInHeader'/>
+              <button className='searchButtonInHeader'><AiOutlineSearch/></button>
+            </div>
             <div><button>Wallet connect</button></div>
             <div className='iconContainer'><FiBell/></div>
-            <div className='iconContainer' onClick={handleHeaderAndMenu}><RxHamburgerMenu/></div>
+            <div className='iconContainer' id='hamburgerIcon' onClick={handleHeaderAndMenu}><RxHamburgerMenu/></div>
             <div className='iconContainer'><FiSun/></div>
         </section>
-      </header>}
+      </header>
+
+      <ul className='homePagesList' id='homePagesList'>
+        <li>Home Page One</li>
+        <li>Home Page Two</li>
+        <li>Home Page Three</li>
+        <li>Home Page Four</li>
+        <li>Home Page Five</li>
+      </ul>
+      </div>}
 
       {showSearchBar && 
       <div className='searchBarContainer'>
