ab6d42f8b8a6e1e0462a3c6935212e4d203bfbf6
Tatiana Cardona <clleidytatina@gmail.com>
2023-07-25 21:34:01 -0500

    feat: adding data in the cards component

--- src/Components/CardExplore/index.jsx
+++ src/Components/CardExplore/index.jsx
@@ -1,25 +1,43 @@
-import { FaUserCircle } from 'react-icons/fa';
+import { useEffect, useState } from 'react';
 import Card from '../Card';
 import './CardExplore.scss';
+import cardData from '../../assets/data';
 
-const CardExplore = () => (
-  <div className="card-secction">
-    <div className="container-2">
-      <div className="row-1">
-        <div className="">
-          <div className="cards">
-            <Card nftName="Juan" price={1234} nftImage="../public/design2.webp" profileImage={<FaUserCircle />} placeBit={89} address="#" />
-            <Card nftName="Juan" price={1234} nftImage="../public/design2.webp" profileImage={<FaUserCircle />} placeBit={89} address="#" />
-            <Card nftName="Juan" price={1234} nftImage="../public/design2.webp" profileImage={<FaUserCircle />} placeBit={89} address="#" />
-            <Card nftName="Juan" price={1234} nftImage="../public/design2.webp" profileImage={<FaUserCircle />} placeBit={89} address="#" />
-            <Card nftName="Juan" price={1234} nftImage="../public/design2.webp" profileImage={<FaUserCircle />} placeBit={89} address="#" />
-            <Card nftName="Juan" price={1234} nftImage="../public/design2.webp" profileImage={<FaUserCircle />} placeBit={89} address="#" />
-            <Card nftName="Juan" price={1234} nftImage="../public/design2.webp" profileImage={<FaUserCircle />} placeBit={89} address="#" />
+const CardExplore = () => {
+  const [dataNft, setDataNft] = useState([]);
+
+  useEffect(() => {
+    setDataNft(cardData);
+  }, []);
+  return (
+    <div className="card-secction">
+      <div className="container-2">
+        <div className="row-1">
+          <div className="">
+            <div className="cards">
+              {
+                dataNft.map((nft) => {
+                  return (
+                    <Card
+                      key={nft.id}
+                      totalLikes={nft.totalLikes}
+                      nftName={nft.nftName}
+                      price={nft.price}
+                      nftImage={nft.nftImage}
+                      profileImage1={nft.profileImage1}
+                      profileImage2={nft.profileImage2}
+                      profileImage3={nft.profileImage3}
+                      placeBit={nft.placeBit}
+                    />
+                  );
+                })
+              }
+            </div>
           </div>
         </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default CardExplore;
