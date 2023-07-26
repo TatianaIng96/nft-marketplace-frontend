478380800ea1ad6bf01aa98c2c93b9325a3be009
Tatiana Cardona <104793222+TatianaIng96@users.noreply.github.com>
2023-07-26 16:37:25 -0500

    Profile (#7)
    * feat: feacture cover and profile photo in profile page
    
    * feat: add card in profile page
    
    * feat: profile page with components
    
    * feat: button id in profile
    
    * feat: buttn function

---
+++ src/Components/AuthorInner/index.jsx
@@ -0,0 +1,19 @@
+import '../../pages/Profile/Profile.scss';
+
+function AuthorInner() {
+  return (
+    <img
+      alt="MRS SUNAYRA AHSAN"
+      srcSet="https://nuron-nextjs.vercel.app/_next/image?url=%2Fimages%2Fslider%2Fbanner-06.png&amp;w=256&amp;q=75 1x,
+      https://nuron-nextjs.vercel.app/_next/image?url=%2Fimages%2Fslider%2Fbanner-06.png&amp;w=384&amp;q=75 2x"
+      src="https://nuron-nextjs.vercel.app/_next/image?url=%2Fimages%2Fslider%2Fbanner-06.png&amp;w=384&amp;q=75"
+      width="140"
+      height="140"
+      decoding="async"
+      data-nimg="1"
+      loading="lazy"
+      className="img-profile"
+    />
+  );
+}
+export default AuthorInner;
