49a7c661835196de4792e2bf3b638a2eb7dda8f9
Camilo-Suarez98 <camilo.suarez85@gmail.com>
2023-07-31 12:31:21 -0500

    feat: correction our-collection-page

--- src/pages/OurCollection/index.jsx
+++ src/pages/OurCollection/index.jsx
@@ -10,12 +10,14 @@ const OurCollection = () => {
       <div className="cards-collection-container">
         <ListOfCardCollection />
       </div>
-      <div className="pagination">
-        <Link className="pagination__button" to="#top">Previous</Link>
-        <Link className="pagination__button-selected" to="#top">1</Link>
-        <Link className="pagination__button" to="#top">2</Link>
-        <Link className="pagination__button" to="#top">3</Link>
-        <Link className="pagination__button" to="#top">Next</Link>
+      <div className="padding-btn-pagination">
+        <div className="pagination">
+          <Link className="pagination__button" to="#top">Previous</Link>
+          <Link className="pagination__button-selected" to="#top">1</Link>
+          <Link className="pagination__button" to="#top">2</Link>
+          <Link className="pagination__button" to="#top">3</Link>
+          <Link className="pagination__button" to="#top">Next</Link>
+        </div>
       </div>
     </div>
   );
