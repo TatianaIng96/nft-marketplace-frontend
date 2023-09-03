f8a54dbb3dfb56fdad8e658b1bf92dc82d379d84
Juan Camilo Clement Santacruz <juancamiloclement@gmail.com>
2023-09-03 13:02:06 -0500

    user can update or delete profile or cover image

--- src/pages/EditProfileImage/index.jsx
+++ src/pages/EditProfileImage/index.jsx
@@ -5,10 +5,10 @@
 /* eslint-disable jsx-a11y/img-redundant-alt */
 /* eslint-disable react/jsx-one-expression-per-line */
 import './EditProfileImage.scss';
-import { useRef, useState } from 'react';
+import { useRef, useState, useEffect } from 'react';
 import { AiOutlineEye } from 'react-icons/ai';
 import { useNavigate } from 'react-router-dom';
-import axios from 'axios';
+// import axios from 'axios';
 import Inner from '../../Components/Inner';
 import EditProfileMenu from '../../Components/EditProfileMenu';
 
@@ -18,6 +18,35 @@ const EditProfileImage = () => {
   const profileImageInputRef = useRef(null);
   const coverImageInputRef = useRef(null);
 
+  const [user, setUser] = useState({});
+  // const [profileImageToRender, setProfileImageToRender] = useState('../../profile-image.png');
+  const [profileImageFile, setProfileImageFile] = useState({});
+  const [coverImageFile, setCoverImageFile] = useState({});
+
+  useEffect(() => {
+    async function fetchUser() {
+      try {
+        const fetchConfig = {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${localStorage.getItem('token')}`,
+          },
+        };
+
+        const response = await fetch(`${import.meta.env.VITE_API_BASE_URL}/api/users/single`, fetchConfig);
+        const fetchedUser = await response.json();
+        setUser(fetchedUser);
+        // if (user.profileImage[0].url) {
+        //   setProfileImageToRender(user.profileImage[0].url);
+        // }
+      } catch (error) {
+        alert({ message: error.message });
+      }
+    }
+    fetchUser();
+  }, []);
+
   const handleProfileImageUpload = () => {
     profileImageInputRef.current?.click();
   };
@@ -25,41 +54,50 @@ const EditProfileImage = () => {
     coverImageInputRef.current?.click();
   };
 
-  const [profileImageFile, setProfileImageFile] = useState({});
-  const [coverImageFile, setCoverImageFile] = useState({});
-
   const handleProfileImageFile = (event) => {
     setProfileImageFile(event.target.files);
+    // setProfileImageToRender(event.target.files);
   };
   const handleCoverImageFile = (event) => {
     setCoverImageFile(event.target.files);
   };
 
   const handleProfileImageSubmit = async (event) => {
+    // await axios.post(
+    //   `${import.meta.env.VITE_API_BASE_URL}/api/profile-image/`,
+    //   data,
+    //   {
+    //     headers: {
+    //       'Content-Type': 'multipart/form-data',
+    //       Authorization: `Bearer ${localStorage.getItem('token')}`,
+    //     },
+    //   },
+    // );
     event.preventDefault();
 
     const data = new FormData();
 
     data.append('url', profileImageFile[0], profileImageFile[0].name);
 
-    // const fetchConfigForm = {
-    //   method: 'POST',
-    //   body: data,
-    //   headers: {
-    //     'Authorization': `Bearer ${localStorage.getItem('token')}`,
-    //     'Content-Type': 'multipart/form-data',
-    //   },
-    // };
-
-    await axios.post(
-      `${import.meta.env.VITE_API_BASE_URL}/api/profile-image/`,
-      data,
-      {
-        headers: {
-          'Content-Type': 'multipart/form-data',
-          Authorization: `Bearer ${localStorage.getItem('token')}`,
-        },
+    const fetchConfigPost = {
+      method: 'POST',
+      body: data,
+      headers: {
+        'Authorization': `Bearer ${localStorage.getItem('token')}`,
       },
+    };
+
+    const fetchConfigPut = {
+      method: 'PUT',
+      body: data,
+      headers: {
+        'Authorization': `Bearer ${localStorage.getItem('token')}`,
+      },
+    };
+
+    await fetch(
+      `${import.meta.env.VITE_API_BASE_URL}/api/profile-image/`,
+      user.profileImage[0] ? fetchConfigPut : fetchConfigPost,
     );
 
     navigate('/my-profile');
@@ -72,7 +110,7 @@ const EditProfileImage = () => {
 
     data.append('url', coverImageFile[0], coverImageFile[0].name);
 
-    const fetchConfigForm = {
+    const fetchConfigPost = {
       method: 'POST',
       body: data,
       headers: {
@@ -80,7 +118,44 @@ const EditProfileImage = () => {
       },
     };
 
-    await fetch(`${import.meta.env.VITE_API_BASE_URL}/api/cover-image/`, fetchConfigForm);
+    const fetchConfigPut = {
+      method: 'PUT',
+      body: data,
+      headers: {
+        'Authorization': `Bearer ${localStorage.getItem('token')}`,
+      },
+    };
+
+    await fetch(
+      `${import.meta.env.VITE_API_BASE_URL}/api/cover-image/`,
+      user.coverImage[0] ? fetchConfigPut : fetchConfigPost,
+    );
+
+    navigate('/my-profile');
+  };
+
+  const handleDeleteCoverImage = async () => {
+    const fetchConfig = {
+      method: 'DELETE',
+      headers: {
+        'Authorization': `Bearer ${localStorage.getItem('token')}`,
+      },
+    };
+
+    await fetch(`${import.meta.env.VITE_API_BASE_URL}/api/cover-image/`, fetchConfig);
+
+    navigate('/my-profile');
+  };
+
+  const handleDeleteProfileImage = async () => {
+    const fetchConfig = {
+      method: 'DELETE',
+      headers: {
+        'Authorization': `Bearer ${localStorage.getItem('token')}`,
+      },
+    };
+
+    await fetch(`${import.meta.env.VITE_API_BASE_URL}/api/profile-image/`, fetchConfig);
 
     navigate('/my-profile');
   };
@@ -100,8 +175,13 @@ const EditProfileImage = () => {
           <section className="optionsSection">
             <div className="profilePicture">
               <span>Change your profile picture</span>
-              <img src="../../profile-image.png" alt="Profile Picture" onClick={handleProfileImageUpload} />
+              <img
+                src="../../profile-image.png"
+                alt="Profile Picture"
+                onClick={handleProfileImageUpload}
+              />
               <button type="button" onClick={handleProfileImageSubmit} className="uploadButton">Upload</button>
+              <button type="button" onClick={handleDeleteProfileImage} className="deleteButton">Delete</button>
               <input
                 type="file"
                 ref={profileImageInputRef}
@@ -113,8 +193,13 @@ const EditProfileImage = () => {
             </div>
             <div className="coverPhoto">
               <span>Change your cover photo</span>
-              <img src="../../nft-background.webp" alt="Cover Photo" onClick={handleCoverImageUpload} />
+              <img
+                src="../../nft-background.webp"
+                alt="Cover Photo"
+                onClick={handleCoverImageUpload}
+              />
               <button type="button" onClick={handleCoverImageSubmit} className="uploadButton">Upload</button>
+              <button type="button" onClick={handleDeleteCoverImage} className="deleteButton">Delete</button>
               <input
                 type="file"
                 ref={coverImageInputRef}
