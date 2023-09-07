7483903b2570488e9a13e6e82e703272d7191c67
Camilo-Suarez98 <camilo.suarez85@gmail.com>
2023-09-06 21:10:51 -0500

    fix error in header with dark mode

--- src/store/ThemeContext.jsx
+++ src/store/ThemeContext.jsx
@@ -6,17 +6,15 @@ const ThemeContext = createContext();
 const ThemeProvider = ({ children }) => {
   const [theme, setTheme] = useState('dark');
 
-  const toggleTheme = () => {
+  const toggletheme = () => {
     setTheme(theme === 'dark' ? 'ligth' : 'dark');
   };
 
-  const backgroundColor = theme === 'dark' ? '#13131d' : '#f5f8fa';
-  const color = theme === 'dark' ? '' : '#13131d';
+  const backgroundColor = theme === 'dark' ? '#13131d' : '#ACACAC';
   document.body.style.backgroundColor = backgroundColor;
-  document.body.style.color = color;
 
   return (
-    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+    <ThemeContext.Provider value={{ theme, toggletheme }}>
       {children}
     </ThemeContext.Provider>
   );
