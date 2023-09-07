0611f835275881b046efaaa8ad5255078d5a597c
Juan Camilo Clement Santacruz <juancamiloclement@gmail.com>
2023-09-07 18:33:32 -0500

    adds access control allow origin header to create profile image request

--- src/pages/EditProfileImage/index.jsx
+++ src/pages/EditProfileImage/index.jsx
@@ -114,6 +114,7 @@ const EditProfileImage = () => {
       body: data,
       headers: {
         'Authorization': `Bearer ${localStorage.getItem('token')}`,
+        'Access-Control-Allow-Origin': 'https://nft-marketplace-frontend-9z7xtntur-tatianaing96.vercel.app/edit-profile-image',
       },
     };
 
