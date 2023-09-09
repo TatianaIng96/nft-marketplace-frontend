3676b9ee8dabdfd371f923c8f3d908111cef834b
Juan Camilo Clement Santacruz <juancamiloclement@gmail.com>
2023-09-09 12:48:43 -0500

    admin can delete user and login verifies actives status

--- src/Components/ModalConfirmAdminRole/index.jsx
+++ src/Components/ModalConfirmAdminRole/index.jsx
@@ -1,16 +1,74 @@
+/* eslint-disable quote-props */
 import './ModalConfirmAdminRole.scss';
+import { useState } from 'react';
+import { useParams } from 'react-router-dom';
 
 const ModalConfirmAdminRole = ({ onCancel }) => {
+  const { id } = useParams();
+
+  const [password, setPassword] = useState({
+    password: '',
+  });
+  const [message, setMessage] = useState('');
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setPassword({
+      ...password,
+      [name]: value,
+    });
+    console.log(password);
+  };
+
+  const handleDelete = async (event) => {
+    event.preventDefault();
+
+    try {
+      const fetchConfig = {
+        method: 'DELETE',
+        body: JSON.stringify(password),
+        headers: {
+          'Authorization': `Bearer ${localStorage.getItem('token')}`,
+        },
+      };
+      const response = await fetch(`${import.meta.env.VITE_API_BASE_URL}/api/users/${id}`, fetchConfig);
+      const data = await response.json();
+      setMessage(data.message);
+    } catch (error) {
+      setMessage(error.message);
+    }
+  };
+
+  if (message !== '') {
+    return <div className="message">{message}</div>;
+  }
+
   return (
     <div className="modalConfirmAdminRole">
-      <form action="">
+      <form onSubmit={handleDelete}>
         <label htmlFor="password">
           Please verify your role as an administrator by confirming your password
-          <input type="password" name="password" id="password" />
+          <input
+            type="password"
+            name="password"
+            id="password"
+            onChange={handleChange}
+          />
         </label>
         <div className="buttonsContainer">
-          <button type="button" onClick={() => { return onCancel(false); }} className="cancelButton">Cancel</button>
-          <button type="submit" className="confirmButton">Confirm</button>
+          <button
+            type="button"
+            onClick={() => { return onCancel(false); }}
+            className="cancelButton"
+          >
+            Cancel
+          </button>
+          <button
+            type="submit"
+            className="confirmButton"
+          >
+            Confirm
+          </button>
         </div>
       </form>
     </div>
--- src/pages/AdminEditUser/index.jsx
+++ src/pages/AdminEditUser/index.jsx
@@ -5,7 +5,6 @@ import { AiOutlineEye } from 'react-icons/ai';
 import { useState, useEffect } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import ModalDeleteUserConfirmation from '../../Components/ModalDeleteUserConfirmation';
-import ModalConfirmAdminRole from '../../Components/ModalConfirmAdminRole';
 import Inner from '../../Components/Inner';
 import EditProfileMenu from '../../Components/EditProfileMenu';
 import UserInfoForm from '../../Components/UserInfoForm';
@@ -16,7 +15,6 @@ const AdminEditUser = () => {
   const { id } = useParams();
 
   const [deleteUserQuestion, setDeleteUserQuestion] = useState(false);
-  const [confirmAdmin, setConfirmAdmin] = useState(false);
   const [user, setUser] = useState({
     firstName: '',
     lastName: '',
@@ -93,7 +91,13 @@ const AdminEditUser = () => {
           <div className="editMenuContainer">
             <EditProfileMenu />
             <div className="deleteUserContainer">
-              <button type="button" onClick={() => { return setDeleteUserQuestion(true); }} className="deleteUserButton">Delete user</button>
+              <button
+                type="button"
+                onClick={() => { return setDeleteUserQuestion(true); }}
+                className="deleteUserButton"
+              >
+                Delete user
+              </button>
             </div>
           </div>
           <div className="userInfoFormContainer">
@@ -105,18 +109,21 @@ const AdminEditUser = () => {
           </div>
         </div>
         <div className="deleteUserContainer">
-          <button type="button" className="deleteUserButton">Delete user</button>
+          <button
+            type="button"
+            onClick={() => { return setDeleteUserQuestion(true); }}
+            className="deleteUserButton"
+          >
+            Delete user
+          </button>
         </div>
       </div>
       {deleteUserQuestion
         && (
           <ModalDeleteUserConfirmation
             onCancel={setDeleteUserQuestion}
-            onDelete={setConfirmAdmin}
           />
         )}
-      {confirmAdmin
-        && <ModalConfirmAdminRole onCancel={setConfirmAdmin} />}
     </div>
   );
 };
