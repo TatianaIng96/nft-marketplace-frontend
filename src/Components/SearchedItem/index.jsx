4580bf46c2cc594dafc715c81c70981ade91b99c
Juan Camilo Clement Santacruz <juancamiloclement@gmail.com>
2023-09-15 11:23:52 -0500

    fixes profile navigation from search item

--- src/Components/SearchedItem/index.jsx
+++ src/Components/SearchedItem/index.jsx
@@ -1,13 +1,17 @@
+/* eslint-disable no-nested-ternary */
 import './SearchedItem.scss';
 import { Link } from 'react-router-dom';
+import { useJwt } from 'react-jwt';
 
 const SearchedItem = ({ element }) => {
+  const { decodedToken } = useJwt(localStorage.getItem('token'));
+
   return (
     <div className="searched-item">
-      <Link to={element.email ? `/profile/${element.id}` : `/product-details/${element.id}`}>
-        <img src={element.image} alt="" />
+      <Link to={element.email ? (element.id === decodedToken?.id ? '/my-profile' : `/profile/${element.id}`) : `/product-details/${element.id}`}>
+        <img src={element.image} alt="Avatar/nft" />
       </Link>
-      <Link to={element.email ? `/profile/${element.id}` : `/product-details/${element.id}`}>
+      <Link to={element.email ? (element.id === decodedToken?.id ? '/my-profile' : `/profile/${element.id}`) : `/product-details/${element.id}`}>
         <h3>{element.name}</h3>
       </Link>
     </div>
